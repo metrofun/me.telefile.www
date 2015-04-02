ac8f57b09fa7ae21c58413f121f8467b40034b2e
Dmytrii Shchadei <gmetrofun@gmail.com>
2015-04-02 13:14:57 +0200

    switched to non buggy rxjs version

--- src/ui/index.js
+++ src/ui/index.js
@@ -17,7 +17,7 @@ if (Env.IS_DEBUG) {
     };
 
     // @see https://github.com/Reactive-Extensions/RxJS/blob/master/doc/gettingstarted/testing.md
-    Rx.config.longStackSupport = true;
+    // Rx.config.longStackSupport = true;
 }
 
 
