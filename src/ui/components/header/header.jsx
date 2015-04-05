5f7d3f7fb4d5280570628909872f1b4400e77e9a
Dmytrii Shchadei <gmetrofun@gmail.com>
2015-04-05 03:19:49 +0200

    added mobile social sharing

--- src/ui/components/header/header.jsx
+++ src/ui/components/header/header.jsx
@@ -20,17 +20,21 @@ class Header extends React.Component {
                     <div className="brand__logo"></div>
                     <div className="brand__file">file</div>
                 </div>
-                <ul className="share" onClick={this._onShare}>
-                    <li className="share__item share__twitter">
-                        <a title="Share on Twitter" target="_blank" href="https://twitter.com/home?status=TeleFile.Me%20%E2%80%93%20free%20tool%20for%20peer-to-peer%20file%20share%20via%20%40TeleFileMe">&nbsp;</a>
-                    </li>
-                    <li className="share__item share__facebook">
-                        <a title="Share on Facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://telefile.me">&nbsp;</a>
-                    </li>
-                    <li className="share__item share__linkedin">
-                        <a title="Share on LinkedIn" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=https://telefile.me&title=TeleFile.Me%20%E2%80%93%20Peer-to-peer%20file%20sharing%20tool&summary=&source=">&nbsp;</a>
-                    </li>
-                </ul>
+                <div className="share">
+                    <input className="share__checkbox" id="share__checkbox" type="checkbox" />
+                    <label className="share__label" htmlFor="share__checkbox"></label>
+                    <ul className="share__list" onClick={this._onShare}>
+                        <li className="share__item share__twitter">
+                            <a className="share__link" title="Share on Twitter" target="_blank" href="https://twitter.com/home?status=TeleFile.Me%20%E2%80%93%20free%20tool%20for%20peer-to-peer%20file%20share%20via%20%40TeleFileMe">&nbsp;</a>
+                        </li>
+                        <li className="share__item share__facebook">
+                            <a className="share__link" title="Share on Facebook" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https://telefile.me">&nbsp;</a>
+                        </li>
+                        <li className="share__item share__linkedin">
+                            <a className="share__link" title="Share on LinkedIn" target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=https://telefile.me&title=TeleFile.Me%20%E2%80%93%20Peer-to-peer%20file%20sharing%20tool&summary=&source=">&nbsp;</a>
+                        </li>
+                    </ul>
+                </div>
                 <ul className="navbar">
                     <li className="navbar__item navbar__item_type_active">Home</li>
                     <li className="navbar__item">About</li>
