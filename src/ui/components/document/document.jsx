dfd0b79d16eda96934a3946803a336b5b3a0aee0
Dmytrii Shchadei <gmetrofun@gmail.com>
2015-05-03 02:24:08 +0200

    added analytics

--- src/ui/components/document/document.jsx
+++ src/ui/components/document/document.jsx
@@ -9,6 +9,13 @@ class DocumentComponent extends React.Component {
             __html: React.renderToString(<Page/>)
         };
     }
+    _getAnalytics() {
+        // hack to set checksum to the content,
+        // so that content can be reused in client side
+        return {
+            __html: "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-9568575-7', 'auto'); ga('send', 'pageview');"
+        };
+    }
     render() {
         return (<html>
             <head>
@@ -53,6 +60,8 @@ class DocumentComponent extends React.Component {
             </head>
             <body dangerouslySetInnerHTML={this._getContent()}></body>
             <script async src="/index.js"></script>
+            <script dangerouslySetInnerHTML={this._getAnalytics()}>
+                </script>
         </html>);
     }
 }
